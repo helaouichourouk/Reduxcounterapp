0320de20b516ee7a76f833365e1ff32d2993a4ce
helaouichourouk <chourouk.helaoui2@gmail.com>
2026-04-06 15:20:59 +0100

    Initial commit - React Redux counter app

---
+++ src/components/Counter.jsx
@@ -0,0 +1,34 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "../features/counter/counterSlice";
+import "../App.css";
+
+function Counter() {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+        <div className="counter">
+            <h1 className="counter__title">Redux Counter 🚀</h1>
+
+            <div className="counter__value">{count}</div>
+
+            <div className="counter__buttons">
+                <button
+                    className="btn btn--decrement"
+                    onClick={() => dispatch(decrement())}
+                >
+                    -
+                </button>
+
+                <button
+                    className="btn btn--increment"
+                    onClick={() => dispatch(increment())}
+                >
+                    +
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
